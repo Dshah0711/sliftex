5d90e718acffb3a24eca60034abbe4f7a810fc06
Dhanvi Shah <dhanvishah0711@gmail.com>
2024-12-12 12:56:59 +0530

    Initial commit

--- src/Routes/Service.jsx
+++ src/Routes/Service.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios'
 import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import Chatbot from '../components_service/chatbot'
+
 import DynamicTable from '../components_service/DynamicTable'
 
 import Footer from '../components/Footer'

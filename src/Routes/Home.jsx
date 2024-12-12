cafbeeeaf0c128e8e9143d06bb3dcba35d742fae
Dhanvi Shah <dhanvishah0711@gmail.com>
2024-12-12 13:48:22 +0530

    Commit

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import axios from 'axios'
-import { useEffect, useState } from 'react'
-import { Link, Navigate, useNavigate } from 'react-router-dom'
+
 import Navbar from "../components/Navbar"
 import Content from '../components/Content'
 import Card from "../components/Card"
@@ -11,37 +10,10 @@ import Footer from '../components/Footer'
 
 const Home = () => {
 
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const navigate = useNavigate();
-  axios.defaults.withCredentials = true;
-
-  useEffect(() => {
-    axios.get('http://localhost:3000/auth/verify')
-    .then(res => {
-      if (res.data.isAuthenticated) {
-        setIsLoggedIn(true);
-      } else {
-        setIsLoggedIn(false);
-      }
-    }).catch(err => {
-      console.log(err);
-    });
-}, []);
-
-const handleLogout = () => {
-  axios.get('http://localhost:3000/auth/logout')
-    .then(res => {
-      if (res.data.status) {
-        setIsLoggedIn(false);
-        navigate('/login');
-      }
-    }).catch(err => {
-      console.log(err);
-    });
-};
+  
   return (
     <>
-     <Navbar isLoggedIn={isLoggedIn} handleLogout={handleLogout}/>
+     <Navbar />
       <Content />
      <ToolsAndTech />
      <StepByStepGuide />
--- src/Routes/Service.jsx
+++ src/Routes/Service.jsx
@@ -1,25 +1,12 @@
 
-import axios from 'axios'
+
 import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
 
 import DynamicTable from '../components_service/DynamicTable'
 
 import Footer from '../components/Footer'
 const Service = () => {
-  const navigate = useNavigate()
-  axios.defaults.withCredentials = true;
-  useEffect(() => {
-      axios.get('http://localhost:3000/auth/verify')
-      .then(res=> {
-          if(res.data.status) {
 
-          } else {
-              navigate('/login')
-          }
-          console.log(res);
-      })
-  }, [])
   return (
     <div>
      
